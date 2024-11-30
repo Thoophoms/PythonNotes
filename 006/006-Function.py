6c785614242c9864a060ecae0f701ae45d1592c2
thoophoms <thoophom.su@gmail.com>
2024-11-30 01:15:23 -0500

    Add 006 Function and Maze codes

---
+++ 006/006-Function.py
@@ -0,0 +1,9 @@
+# Functions
+# Making own dunction
+
+def my_function(name):
+    print(f"Hello, {name}")
+
+
+user_name = input("What is your name?: ")
+my_function(name=user_name)
---
+++ 006/006-Maze_Code.py
@@ -0,0 +1,42 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+# def turn_around():
+#     turn_left()
+#     turn_left()
+#
+#
+# def turn_right():
+#     turn_around()
+#     turn_left()
+#
+#
+# def lets_go():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+#
+# def jump():
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+#
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif wall_in_front():
+#         turn_left()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
+#
