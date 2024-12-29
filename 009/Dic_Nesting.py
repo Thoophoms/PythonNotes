c5da26c7d239c9b14275645466fe0b3d8075690e
Trisha Supannopaj <thoophom.su@gmail.com>
2024-12-30 01:10:41 +0700

    Converts student scores to grades

--- 009/Dic_Nesting.py
+++ 009/Dic_Nesting.py
@@ -34,3 +34,38 @@ this_is_dictionary = {}
 print(this_is_dictionary)
 # Output = {}
 
+
+# Todo: Write a program that converts their scores to grades.
+# Write a new dictionary called student_grades that should contain student names as keys
+# and their assessed grades for values.
+# The final version of the student_grades dictionary will be checked.
+# This is the scoring criteria:
+# - Scores 91 - 100: Grade = "Outstanding"
+# - Scores 81 - 90: Grade = "Exceeds Expectations"
+# - Scores 71 - 80: Grade = "Acceptable"
+# - Scores 70 or lower: Grade = "Fail"
+
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+student_grades = {}
+
+for name in student_scores:
+    if student_scores[name] >= 91:
+        student_grades[name] = "Outstanding"
+    elif student_scores[name] >= 81:
+        student_grades[name] = "Exceeds Expectations"
+    elif student_scores[name] >= 71:
+        student_grades[name] = "Acceptable"
+    else:
+        student_grades[name] = "Fail"
+
+print(student_grades)
+
+
+
