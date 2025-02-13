e4e599de793f4c8e8e93c12a5b67265d9c953316
Trisha Supannopaj <thoophom.su@gmail.com>
2025-02-12 23:54:31 -0500

    Modify the code for calculation

--- 010/calculation.py
+++ 010/calculation.py
@@ -27,23 +27,24 @@ operations = {
 # print(result)
 
 # Todo 4: Functionality
-end_the_program = False
-
-while not end_the_program:
+def calculator():
     continue_operations = True
 
     first_number = float(input("Pick the number: "))
 
     while continue_operations:
-        user_operation = input("Pick the operation\n+\n-\n*\n/\n")
+        # Loop through the operation symbol in operations dictionary
+        for operation_key in operations:
+            print(operation_key)
+        user_operation = input("Pick the operation: ")
         second_number = float(input(f"Pick the number you would like to do the operation: "))
 
         if user_operation in operations:
             result = float(operations[user_operation](first_number, second_number))
-            print(result)
+            print(f"{first_number} {user_operation} {second_number} = {result}")
 
             # Ask if the user wants to continue
-            continue_operation = (input("Would you like to continue working with the previous result? 'y' for Yes or 'n' for No: ")).lower()
+            continue_operation = (input(f"Would you like to continue working with {result}? \n'y' for Yes or 'n' for No: ")).lower()
             if continue_operation == "y":
                 first_number = result
             else:
@@ -54,7 +55,9 @@ while not end_the_program:
     # Ask if the user wants to start over with the new calculation?
     start_again = (input("Would you like to start over the calculation? 'y' for Yes or 'n' for No: ")).lower()
     if start_again == "y":
-        pass
+        calculator()
     else:
         print("Thank you, Bye!")
         end_the_program = True
+
+calculator()
